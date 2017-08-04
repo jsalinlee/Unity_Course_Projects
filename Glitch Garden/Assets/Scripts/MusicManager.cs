f6677efe76d48569c1443c611551244c4fefc17c
jsalinlee <jsalinlee@gmail.com>
2017-08-04 14:56:01 -0700

    Started Glitch Garden project

---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    void OnEnable() {
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    void OnDisable() {
+        SceneManager.sceneLoaded -= OnLevelLoaded;
+    }
+
+    void OnLevelLoaded(Scene scene, LoadSceneMode loadSceneMode) {
+        AudioClip thisLevelMusic = levelMusicChangeArray[scene.buildIndex];
+        if(thisLevelMusic) {
+                audioSource.clip = thisLevelMusic;
+                audioSource.loop = true;
+                audioSource.Play();
+        }
+    }
+}
